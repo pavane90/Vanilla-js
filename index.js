f407d492df5b69cf7991716af6d2996952d0b22a
Byungman Lee <pavane90@gmail.com>
2019-12-02 13:26:55 +0900

    JS DOM Functions

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
+const title = document.getElementById("title");
+console.log(title);
 
-const calculator = {
-    plus: function(arg1, arg2){
-        return arg1+arg2;
-    }
-}
-
-// console.log(greetLee);
-const plus = calculator.plus(5,5);
-console.log(plus);
+title.innerHTML = "ASDF";
