1e1ca2070ad0debb627eb686649b95b049606cbd
Byungman Lee <pavane90@gmail.com>
2019-12-02 11:11:16 +0900

    rebase

--- index.js
+++ index.js
@@ -1,2 +1,44 @@
 console.log("Im working. Im JS. Im beautiful");
 alert("alert");
+// String
+const what = "Takenotsuka";
+
+console.log(what);
+
+// Boolean
+const wat = true;
+console.log(wat);
+
+// Number
+const wt = 10;
+console.log(wt);
+
+// Float
+const w = 55.1;
+console.log(w);
+
+//array & object
+
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+console.log(daysOfWeek);
+console.log(daysOfWeek[2]);
+
+//Object
+
+const leeInfo = {
+    name : "lee",
+    age : 29,
+    gender: "Male",
+    favMovies: ["Matrix", "LOTR", "Avengers"],
+    favFood: [
+        {
+            name: "Cheese burger",
+            price: 1300
+        },
+    ]
+};
+
+leeInfo.age = 30;
+
+console.log(leeInfo);
