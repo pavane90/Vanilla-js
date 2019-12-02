4e2a2d24c7846879e41207c39dd988ed348bb354
Byungman Lee <pavane90@gmail.com>
2019-12-02 13:34:21 +0900

    Modifying the DOM with JS

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const title = document.getElementById("title");
-console.log(title);
-
-title.innerHTML = "ASDF";
+const title = document.querySelector("#title");
+title.innerHTML = "HI! From JS";
+title.style.color = "red";
+document.title= "I own you now"
