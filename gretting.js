245487003316c5d6deecb803247fbdac7444da9e
Byungman Lee <pavane90@gmail.com>
2019-12-02 15:43:06 +0900

    Saving the User Name part Two

--- gretting.js
+++ gretting.js
@@ -1,20 +1,37 @@
 const form = document.querySelector(".js-form"),
 input = form.querySelector("input"),
-gretting = document.querySelector("js-greetings");
+greeting = document.querySelector(".js-greetings");
 
 const USER_LS = "currentUser",
 SHOWING_CN = "showing";
 
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
-    gretting.classList.add(SHOWING_CN);
-    gretting.innerText = `hello ${text}`;
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
+}
+
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
 }
 
 function loadName() {
-    const currentUser = localStorage.getItem(USER_LS);
+    const currentUser = localStorage.getItem(USER_LS); //이름 찾기
     if (currentUser === null) {
         //he is not
+        askForName();
     } else {
         //he is
         paintGreeting(currentUser);
